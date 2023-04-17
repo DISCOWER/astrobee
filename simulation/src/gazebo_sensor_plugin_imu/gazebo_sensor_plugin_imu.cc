61cac13b3b2df775e23b5bfeb0fa6f609b451321
Pedro Roque <padr@kth.se>
2023-04-17 22:25:36 +0200

    add: IMU plugin, to be test...

--- simulation/src/gazebo_sensor_plugin_imu/gazebo_sensor_plugin_imu.cc
+++ simulation/src/gazebo_sensor_plugin_imu/gazebo_sensor_plugin_imu.cc
@@ -16,12 +16,19 @@
  * under the License.
  */
 
+#define PX4_SITL
 
 // IMU Sensor message
 #include <sensor_msgs/msg/imu.hpp>
 
 // Sensor plugin interface
 #include <astrobee_gazebo/astrobee_gazebo.h>
+
+#ifdef PX4_SITL
+  #include <Imu.pb.h>
+  #include <gazebo/transport/transport.hh>
+  #include <gazebo/msgs/msgs.hh>
+#endif
 
 // STL includes
 #include <string>
@@ -71,6 +78,21 @@ class GazeboSensorPluginImu : public FreeFlyerSensorPlugin {
       gzerr << "GazeboSensorPluginImu requires an imu sensor.\n";
       return;
     }
+    # ifdef PX4_SITL
+    // Create Gazebo Transport Node handler
+    std::string namespace_;
+    if (_sdf->HasElement("robotNamespace"))
+      namespace_ = _sdf->GetElement("robotNamespace")->Get<std::string>();
+    else
+      FF_FATAL("GazeboSensorPluginImu requires a robotNamespace.");
+    node_handle_ = transport::NodePtr(new transport::Node());
+    node_handle_->Init(namespace_);
+    imu_pub_ = node_handle_->Advertise<sensor_msgs::msgs::Imu>("/imu", 10);
+    // Set covariances (this is ground truth...)
+    imu_message_.add_angular_velocity_covariance(0.0);
+    imu_message_.add_orientation_covariance(-1.0);
+    imu_message_.add_linear_acceleration_covariance(0.0);
+    # endif
     // Get the name of the link to which this node is attached
     msg_.header.frame_id = GetFrame();
   }
@@ -103,6 +125,38 @@ class GazeboSensorPluginImu : public FreeFlyerSensorPlugin {
     msg_.linear_acceleration.y = imu_->LinearAcceleration().Y();
     msg_.linear_acceleration.z = imu_->LinearAcceleration().Z();
     pub_->publish(msg_);
+
+    #ifdef PX4_SITL
+    // Copy Eigen::Vector3d to gazebo::msgs::Vector3d
+    gazebo::msgs::Vector3d* linear_acceleration = new gazebo::msgs::Vector3d();
+    linear_acceleration->set_x(msg_.linear_acceleration.x);
+    linear_acceleration->set_y(msg_.linear_acceleration.y);
+    linear_acceleration->set_z(msg_.linear_acceleration.z);
+
+    // Copy Eigen::Vector3d to gazebo::msgs::Vector3d
+    gazebo::msgs::Vector3d* angular_velocity = new gazebo::msgs::Vector3d();
+    angular_velocity->set_x(msg_.angular_velocity.x);
+    angular_velocity->set_y(msg_.angular_velocity.y);
+    angular_velocity->set_z(msg_.angular_velocity.z);
+
+    // Fill IMU message.
+    // ADD HEaders
+    // imu_message_.header.stamp.sec = current_time.sec;
+    // imu_message_.header.stamp.nsec = current_time.nsec;
+    imu_message_.set_time_usec(_ msg_.header.stamp.sec * 1000000 + msg_.header.stamp.nanosec / 1000);
+    imu_message_.set_seq(seq_++);
+
+    // Orientation
+    imu_message_.orientation.w = msg_.orientation.w;
+    imu_message_.orientation.x = msg_.orientation.x;
+    imu_message_.orientation.y = msg_.orientation.y;
+    imu_message_.orientation.z = msg_.orientation.z;
+
+    imu_message_.set_allocated_linear_acceleration(linear_acceleration);
+    imu_message_.set_allocated_angular_velocity(angular_velocity);
+
+    imu_pub_->Publish(imu_message_);
+    #endif
   }
 
  private:
@@ -114,6 +168,13 @@ class GazeboSensorPluginImu : public FreeFlyerSensorPlugin {
   event::ConnectionPtr update_;
   // Intermediary data
   sensor_msgs::Imu msg_;
+
+  # ifdef PX4_SITL
+  transport::NodePtr node_handle_;
+  transport::PublisherPtr imu_pub_;
+  sensor_msgs::msgs::Imu imu_message_;
+  uint64_t seq_ = 0;
+  # endif
 };
 
 GZ_REGISTER_SENSOR_PLUGIN(GazeboSensorPluginImu)
