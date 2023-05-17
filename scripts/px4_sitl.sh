decdb88484e57e4af398f3d297f3f2c3000325df
Pedro Roque <padr@kth.se>
2023-05-17 20:44:42 +0200

    fix: adding SITL setup

---
+++ scripts/px4_sitl.sh
@@ -0,0 +1,4 @@
+export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:/src/astrobee/src/external/PX4/build/px4_sitl_default/build_gazebo-classic
+export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/src/astrobee/src/external/PX4/Tools/simulation/gazebo-classic/sitl_gazebo-classic/models
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/src/astrobee/src/external/PX4/build/px4_sitl_default/build_gazebo-classic
+export PX4_ROOT=/src/astrobee/src/external/PX4
